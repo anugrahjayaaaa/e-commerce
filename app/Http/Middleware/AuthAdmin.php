5615f031cce78e724fbebb945225440438619487
Anugrah Jaya <30459937+anugrahjayaaaa@users.noreply.github.com>
2026-07-09 16:05:44 +0700

    implement authadmin middleware

---
+++ app/Http/Middleware/AuthAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->utype === 'ADM') {
+            return $next($request);
+        } else {
+            return redirect()
+                ->route('login')
+                ->with('error', 'You must be an adminisitrator to access this page.');
+        }
+    }
+}
