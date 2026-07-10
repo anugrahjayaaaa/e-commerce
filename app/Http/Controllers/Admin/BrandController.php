fcb4356c99bef42c8916db22a5daff24ee37e044
Anugrah Jaya <30459937+anugrahjayaaaa@users.noreply.github.com>
2026-07-10 16:59:24 +0700

    implement store function, add custom service to controller

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -5,11 +5,21 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Brand\StoreBrandRequest;
 use App\Models\Brand;
+use App\Services\ImageService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class BrandController extends Controller
 {
+
+    protected $imageService;
+
+    // Auto inject by Laravel for image service
+    public function __construct(ImageService $imageService)
+    {
+        $this->imageService = $imageService;
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -30,24 +40,38 @@ class BrandController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreBrandRequest $request)
+    public function store(StoreBrandRequest $request, ImageService $imageService)
     {
+        // Retrieve the validated input data
         $validatedData = $request->validated();
 
         $brand = new Brand();
-        $brand->name = $validatedData->name;
-        $brand->slug = $validatedData->slug ? Str::slug($validatedData->slug) : Str::slug($validatedData->name);
-        $brand->status = $validatedData->has('status') ? 1 : 0;
+        $brand->name = $validatedData['name'];
 
-        if ($validatedData->hasFile('image')) {
-            $imageName = time() . "_" . uniqid() . "." . $validatedData->image->extension();
-            $validatedData->image->move(public_path('uploads/brands/' . $brand->slug), $imageName);
+        // Generate slug from the provided input or fallback to name
+        $brand->slug = !empty($request->slug) ? Str::slug($request->slug) : Str::slug($validatedData['name']);
+
+        // Set status: 1 if active, 0 otherwise
+        $brand->status = $request->has('status') ? 1 : 0;
+
+        // Handle image upload if present
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $imageName = time() . "_" . uniqid() . "." . $file->getClientOriginalExtension();
+
+            // Generate thumbnail using the ImageService
+            $imageService->generateThumbnailImage($file, $imageName, 'uploads/brands', 124, 124);
+
+            // Move the original image to the public storage
+            $file->move(public_path('uploads/brands'), $imageName);
+
             $brand->image = $imageName;
         }
 
         $brand->save();
 
-        return redirect()->route('admin.brands.index')->with('success', 'Brand added successfully!');
+        // Redirect to the index page with a success message
+        return redirect()->route('admin.brands.index')->with('success', 'Brand created successfully!');
     }
 
     /**
