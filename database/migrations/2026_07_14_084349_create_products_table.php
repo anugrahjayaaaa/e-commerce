a2058481674c259985ca7ad4bb6ca8650415220c
Anugrah Jaya <30459937+anugrahjayaaaa@users.noreply.github.com>
2026-07-14 16:39:08 +0700

    update columns

--- database/migrations/2026_07_14_084349_create_products_table.php
+++ database/migrations/2026_07_14_084349_create_products_table.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('short_description')->nullable();
+            $table->text('information')->nullable();
+            $table->text('description');
+            $table->decimal('regular_price', 10, 2);
+            $table->decimal('sale_price', 10, 2)->nullable();
+            $table->string('SKU')->unique()->index();
+            $table->enum('stock_status', ['instock', 'outofstock'])->default('instock');
+            $table->boolean('featured')->default(false);
+            $table->unsignedInteger('quantity')->default(10);
+            $table->string('image')->nullable();
+            $table->text('images')->nullable();
+            $table->foreignId('category_id')->nullable()->constrained('categories')->onDelete('cascade');
+            $table->foreignId('brand_id')->nullable()->constrained('brands')->onDelete('cascade');
+            $table->boolean('status')->default(false);
             $table->timestamps();
         });
     }
