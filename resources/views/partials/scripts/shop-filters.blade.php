cd53c0d797346d964a54f992961599219637cb6d
Anugrah Jaya <30459937+anugrahjayaaaa@users.noreply.github.com>
2026-07-16 15:26:50 +0700

    rename and add .filter-checkbox

--- resources/views/partials/scripts/shop-filters.blade.php
+++ resources/views/partials/scripts/shop-filters.blade.php
@@ -1,7 +1,7 @@
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         const form = document.getElementById('form-filter');
-        document.querySelectorAll('.auto-submit').forEach(element => {
+        document.querySelectorAll('.auto-submit, .filter-checkbox').forEach(element => {
             element.addEventListener('change', () => { 
                 form.submit();
             });
