f6c7fa7086ccd5af9a24d3cd578aefb1813bc31d
Anugrah Jaya <30459937+anugrahjayaaaa@users.noreply.github.com>
2026-07-16 11:19:28 +0700

    implement index shop layout

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Route;
@@ -15,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+// shop
+Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
